e4d66353ad06d8ab56b679f320b6c0aa98861bad
MalaberCodex <malaberlp@icloud.com>
2026-03-17 19:46:36 +0100

    Switch password hashing scheme away from bcrypt on Python 3.14

--- app/core/security.py
+++ app/core/security.py
@@ -7,9 +7,8 @@ from passlib.context import CryptContext
 from app.core.config import settings
 
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["pbkdf2_sha256"],
     deprecated="auto",
-    bcrypt__truncate_error=False,
 )
 
 
